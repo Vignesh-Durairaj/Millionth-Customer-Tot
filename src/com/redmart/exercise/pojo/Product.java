919e8e89f36bc2cbadbc790d4047ee57ab1b7bf7
Vignesh Durairaj <vignesh87.srkv@gmail.com>
2019-02-27 20:59:28 +0530

    Added initial POJO objects

---
+++ src/com/redmart/exercise/pojo/Product.java
@@ -0,0 +1,68 @@
+package com.redmart.exercise.pojo;
+
+public class Product {
+
+	private int productId;
+	
+	private int price;
+	
+	private int length;
+	
+	private int width;
+	
+	private int height;
+	
+	private int weight;
+
+	public int getProductId() {
+		return productId;
+	}
+
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getLength() {
+		return length;
+	}
+
+	public void setLength(int length) {
+		this.length = length;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	
+	public long getVolume() {
+		return this.length * this.width * this.height;
+	}
+}

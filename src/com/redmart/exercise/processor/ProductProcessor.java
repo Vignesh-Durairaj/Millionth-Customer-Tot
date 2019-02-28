ca059f5f71c6c852f286e988c616822dd1214117
Vignesh Durairaj <vignesh87.srkv@gmail.com>
2019-02-28 21:54:25 +0530

    Initial draft solution

--- src/com/redmart/exercise/processor/ProductProcessor.java
+++ src/com/redmart/exercise/processor/ProductProcessor.java
@@ -4,12 +4,18 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import com.redmart.exercise.pojo.Product;
 import com.redmart.exercise.pojo.Tote;
+import com.redmart.exercise.pojo.ToteContent;
 
 public class ProductProcessor {
 
@@ -48,5 +54,48 @@ public class ProductProcessor {
 					prod.getWidth() <= tote.getWidth() && 
 					prod.getHeight() <= tote.getHeight()))
 				.collect(Collectors.toList());
+	}
+	
+	public ToteContent getMaxValueItems(final List<Product> productList, final Tote tote) {
+		Map<Long, ToteContent> toteContentsMap = new HashMap<>();
+		ToteContent emptyToteContent = new ToteContent();
+		emptyToteContent.setItemList(new ArrayList<Integer>());
+		toteContentsMap.put(tote.getVolume(), emptyToteContent);
+		
+		for (Product product : productList) {
+			Map<Long, ToteContent> tempContentsMap = new HashMap<>();
+			tempContentsMap.putAll(toteContentsMap);
+			
+			for (long volume : tempContentsMap.keySet()) {
+				long modifiedVolume = volume - product.getVolume();
+				if(modifiedVolume >= 0) {
+					ToteContent newToteContent = getToteAggregateContent(toteContentsMap.get(volume), product);
+					if (!toteContentsMap.containsKey(modifiedVolume)) {
+						toteContentsMap.put(modifiedVolume, newToteContent);
+					} else {
+						ToteContent existingToteContent = toteContentsMap.get(modifiedVolume);
+						if (existingToteContent.getPrice() < newToteContent.getPrice() || 
+								(existingToteContent.getPrice() == newToteContent.getPrice() 
+								&& existingToteContent.getWeight() >= newToteContent.getWeight())) {
+							toteContentsMap.put(modifiedVolume, newToteContent);
+						}
+					}
+				}
+			}
+		}
+		
+		return Collections.max(toteContentsMap.values(), Comparator.comparingInt(ToteContent::getPrice));
+	}
+	
+	private ToteContent getToteAggregateContent (final ToteContent existingContent, final Product product) {
+		List<Integer> itemsList = new ArrayList<Integer>(existingContent.getItemList());
+		itemsList.add(product.getProductId());
+		
+		ToteContent toteContent = new ToteContent();
+		toteContent.setPrice(existingContent.getPrice() + product.getPrice());
+		toteContent.setWeight(existingContent.getWeight() + product.getWeight());
+		toteContent.setItemList(itemsList);
+		
+		return toteContent;
 	}
 }
